897aac972ac93139b5f92fcb6a7c7c96eb4b8302
Artsiom Biba <biba.artsiom@gmail.com>
2020-09-30 08:16:26 -0400

    adding messages starring

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,7 +3,7 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 class MessagesHeader extends React.Component {
     render() {
-        const { channelName, numUniqueUsers, handleSearchChange, searchLoading, isPrivateChannel } = this.props;
+        const { channelName, numUniqueUsers, handleSearchChange, searchLoading, isPrivateChannel, handleStar, isChannelStarred } = this.props;
 
         return (
             <Segment clearing>
@@ -16,7 +16,13 @@ class MessagesHeader extends React.Component {
                 >
                     <span>
                         {channelName}
-                        {!isPrivateChannel && <Icon name={"star outline"} color="black" />}
+                        {!isPrivateChannel && (
+                            <Icon
+                                onClick={handleStar}
+                                name={isChannelStarred ? 'star' : 'star outline'}
+                                color={isChannelStarred ? 'yellow' : 'black'}
+                            />
+                        )}
                     </span>
                     <Header.Subheader>{numUniqueUsers}</Header.Subheader>
                 </Header>
